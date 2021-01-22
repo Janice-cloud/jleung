924017566b7f252e1a79329fad038d8077f0b85e
Janice Leung <janice.leung2008@gmail.com>
2021-01-22 16:54:23 +1100

    up date mobile home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Jumbotron from "../Jumbotron/index";
+import "../Jumbotron/style.css";
 
 function Home() {
   return (
     <div>
         
         <Jumbotron>
+          <Container>
           <h1 className="display-3 font-weight-bold text-Dark">JANICE</h1>
           <p className="lead text-Dark font-weight-bold">
             Bringing ideas to life!
           </p>
+          </Container>
         </Jumbotron>
      
     </div>
